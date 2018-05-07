8b3826baa3d420c9f8b058ee169f56b6d27c3f3c
Emily Newman <empatnewman@gmail.com>
2018-05-07 17:27:08 -0400

    Fixed Dials - it runs, possibly correctly? is very slow due to looping through all the buckets

--- sim.c
+++ sim.c
@@ -7,8 +7,8 @@
 #define REG_WEIGHT 2
 #define MAX_WEIGHT 4
 
-#define Z_POS  4
-#define Z_NEG  5
+#define Z_POS 4
+#define Z_NEG 5
 
 #define OMP1 0
 #define OMP2 0
@@ -51,15 +51,21 @@ static int next_move(state_t *s, int drone_id) {
      *
      */
     //TODO protect against inf loops
-
+    double total_direction_time = 0;
+    double total_bucket_time = 0;
     // for now: do the naive way
+    double start_time = currentSeconds();
     int start_node = s->drone_position[drone_id];
     int goal_node = s->drone_goal[drone_id];
     int cur_node = start_node;
     // mark all nodes as unvisited
     memset(s->unvisited_nodes, true, g->nnode * sizeof(bool));
     // set distance value to infinity
-    memset(s->node_dist_vals, -1, g->nnode * sizeof(int));
+    //memset(s->node_dist_vals, -1, g->nnode * sizeof(int));
+    int INF = s->max_buckets;
+    for (int i = 0; i < g->nnode; i++) {
+        s->node_dist_vals[i] = INF;
+    }
     // preallocate bucket * nnode lists
     // TODO add to s
     //s->buckets[bucket_num][nnode]
@@ -74,28 +80,32 @@ static int next_move(state_t *s, int drone_id) {
     int cur_bucket = 0;
     s->buckets[0 + start_node] = 1;
     s->bucket_counter[0] = 1;
-    s->node_dist_vals[start_node] = 0;
     s->bucket_index[0] = start_node;
-    printf("hi\n");
+    //printf("hi\n");
+    printf("Time initializing: %.4f\n", currentSeconds() - start_time);
     while (1) {
+        double start_time2 = currentSeconds();
         // Check buckets until first non-empty bucket is found
         bool loop_exit = true;
         for (int i = 0; i < s->max_buckets; i++) {
             if (s->bucket_counter[i] != 0) {
                 cur_bucket = i;
                 loop_exit = false;
-                printf("1. cur-bucket and count: %d %d\n", cur_bucket, s->bucket_counter[i]);
+                //printf("1. cur-bucket and count: %d %d\n", cur_bucket, s->bucket_counter[i]);
                 break;
             }
         }
         if (loop_exit) {
             break;
         }
-        printf("2. cur-bucket and count: %d %d\n", cur_bucket, s->bucket_counter[cur_bucket]);
+        //printf("2. cur-bucket and count: %d %d\n", cur_bucket, s->bucket_counter[cur_bucket]);
 
         // Take first vertex from bucket.
         int cur_vertex = s->bucket_index[cur_bucket];
-        printf("cur_vertex: %d\n", cur_vertex);
+        //printf("cur_vertex: %d\n", cur_vertex);
+        // Remove from bucket
+        s->buckets[cur_bucket * g->nnode + cur_vertex] = 0;
+        s->bucket_counter[cur_bucket]--;
         // current vertex is first index that isn't 0?
         //int cur_vertex = s->buckets[cur_bucket][index];
         //s->bucket_index[cur_bucket]++;
@@ -103,78 +113,79 @@ static int next_move(state_t *s, int drone_id) {
         // Process all adjacents of vertex cur_vertex, and update dist. if required
         for (int d = 0; d < DIRECTIONS; d++) {
             // get neighbor
-            int nbr = calculate_neighbor(cur_vertex, d, g);
-            if (nbr < 0) {
+            int nbr = calculate_neighbor(cur_vertex, (enum direction) d, g);
+            if (nbr < 0 || nbr >= g->nnode) {
                 continue;
             }
             int w = get_weight(d);
 
-            printf("cur_vertex, nbr: %d, %d\n", cur_vertex, nbr);
+            //printf("cur_vertex, nbr: %d, %d\n", cur_vertex, nbr);
             int d_cur = s->node_dist_vals[cur_vertex];
             int d_nbr = s->node_dist_vals[nbr];
-            printf("d_cur, d_nbr, w: %d, %d, %d\n", d_cur, d_nbr, w);
+            //printf("d_cur, d_nbr, w: %d, %d, %d\n", d_cur, d_nbr, w);
             // if this is a shorter path
-            if (d_nbr > d_cur + w || d_nbr == -1) {
-                // if d_nbr isn't INF then it must be in B[dv] bucket, so
+            if (d_nbr > d_cur + w) {
+                // if d_nbr isn't INF then it must be in B[d_nbr] bucket, so
                 // "erase" entry in bucket
-                if (d_nbr != -1) {
-                    *(s->buckets + d_nbr * s->max_buckets + nbr) = 0;
+                if (d_nbr != INF) {
+
+                    s->buckets[d_nbr * g->nnode + nbr] = 0;
                     //s->buckets[d_nbr][nbr] = 0;
-                    s->buckets[d_nbr]--;
+                    s->bucket_counter[d_nbr]--;
                 }
                 // update distance
                 s->node_dist_vals[nbr] = d_cur + w;
                 d_nbr = d_cur + w;
                 // put nbr into updated distance bucket
                 //s->buckets[d_nbr][nbr] = 1;
-                printf("index: %d\n", d_nbr*s->max_buckets + nbr);
-                printf("size: %d\n", s->max_buckets * s->g->nnode);
-                s->buckets[d_nbr * s->max_buckets + nbr] = 1;
+                //printf("index: %d\n", d_nbr*s->max_buckets + nbr);
+                s->buckets[d_nbr * g->nnode + nbr] = 1;
                 s->bucket_counter[d_nbr]++;
+                //printf("test\n");
                 // "storing updated iterator in dist[v].second"???
                 // unnecessary since we iterate thru DIRECTIONS on every node
             }
         }
         // update bucket_index
         // TODO ??
+        total_direction_time += (currentSeconds() - start_time2);
+        start_time2 = currentSeconds();
         for (int b = 0; b < s->max_buckets; b++) {
             int node = 0;
             for (int j = 0; j < s->g->nnode; j++) {
                 // s->buckets[b][j]
-                if (*(s->buckets + b * s->max_buckets + j) != 0) {
+                if (s->buckets[b * g->nnode + j] != 0) {
                     node = j;
                     break;
                 }
             }
             s->bucket_index[b] = node;
         }
+        total_bucket_time += (currentSeconds() - start_time2);
     }
-
+    double end_time = currentSeconds();
+    printf("Time running Dial's: %.4f\n", end_time - start_time);
+    printf("Time spent in directions: %.4f\n", total_direction_time);
+    printf("Time spent in buckets: %.4f\n", total_bucket_time);
+    //printf("end\n\n");
     // get the next move
     // iterate through all the neighbors of the goal, follow the minimum distance
     int result_pos = 0;
     cur_node = goal_node;
+    start_time = currentSeconds();
     while (cur_node != start_node) {
         //printf("cur_node 2: %d\n", cur_node);
         int prev_node = cur_node;
         int min_w = g->nnode+1;
         int min_nbr = 0;
-#if OMP1
-        #pragma omp parallel for
-#endif
         for (int d = 0; d < DIRECTIONS; d++) {
             //enum direction d = i;
             int nbr = calculate_neighbor(cur_node, d, g);
             if (s->node_dist_vals[nbr] < min_w && s->node_dist_vals[nbr] != -1 && nbr != -1) {
                 //printf("min? %d node: %d\n", s->node_dist_vals[nbr], cur_node);
-                #if OMP1
-                # pragma omp critical
-                #endif
 
-                {
                 min_w = s->node_dist_vals[nbr];
                 min_nbr = nbr;
-                }
                 //printf("minimum: %d at node: %d\n", min_w, min_nbr);
             }
         }
@@ -186,7 +197,8 @@ static int next_move(state_t *s, int drone_id) {
         }
 
     }
-
+    end_time = currentSeconds();
+    printf("Time finding move: %.4f\n", end_time - start_time);
     return result_pos;
 }
 
